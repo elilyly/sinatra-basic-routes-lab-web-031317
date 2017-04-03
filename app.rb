c3ea38a0260e9f00816ebc8243f3fea83284ddf6
Elizabeth <elizabethnich19@gmail.com>
2017-04-03 18:00:40 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/:name' do
+    @name = params["name"]
+    erb :index
+  end
+
+  get '/hometown' do
+    @hometown = params["hometown"]
+    erb :hometown
+  end
+
+  get '/favorite-song' do
+    @song = params["song"]
+    erb :favorite_song
+  end
+
 end
